c14a14ea335fdac9ce9803e7abff5bbdf37ea8ee
nguyentien554 <22t1020759@husc.edu.vn>
2026-02-04 07:45:42 +0700

    nguyentien554

---
+++ TEAM_07/ESP32_DHT_OLED/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+THÔNG TIN NHÓM 7
+1. 
+2. 
+3. 
+4. Nguyễn Phan Thủy Tiên
+*/
+
+#include <Arduino.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
